af608cc67727d73be684f775bb658c7038256459
Justin T <justintcoop@gmail.com>
2016-04-08 12:48:26 -0400

    Update wage.cpp

--- wage.cpp
+++ wage.cpp
@@ -1,23 +1,13 @@
 #include <iostream>
 #include <string>
-#include <sstream>
-// #include <map>
 
 using namespace std;
-
-// A: 65
-// G: 71
-// W: 87
-//map<int, int> whoWins = {{-22, -87}, {22, 87}, {-6, 65}, {6, -65}, {-16, 71}, {16, -71}, {0, 0}};
 
 // void afterMath(int& row, int& column, int& numDays, char** battlefield) {
 void afterMath(int& row, int& column, int& numDays, int* diff) {
 	// Instantiate room for battlefield and battlefield on next day
 	char battlefield[row][column];
 	char nextDayMath[row][column];
-	// for (int i = 0; i < 45; ++i) {
-	// 	cout << diff[i] << " ";
-	// }
 	int i, j, charAtCell;
 	// Create the initial battlefield
 	for (i = 0; i < row; ++i) {
@@ -31,8 +21,6 @@ void afterMath(int& row, int& column, int& numDays, int* diff) {
 		// Traverse field to see change in value at end of each day
 		for (i = 0; i < row; ++i) {
 			for (j = 0; j < column; ++j) {
-				// Return negative means that the initial cell is changed
-				// Return positive means that the relative cell to initial cell is changed
 				// If there is a cell below
 				if (i < row - 1) {
 					// Change initial cell at (i, j)
@@ -71,9 +59,10 @@ void afterMath(int& row, int& column, int& numDays, int* diff) {
 }
 
 int main() {
-	//ios::sync_with_stdio(false);
 	int numTestCases;
 	cin >> numTestCases;
+	// Return negative means that the initial cell is changed
+	// Return positive means that the relative cell to initial cell is changed
 	int diff[45];
 	diff[0] = -87;
 	diff[6] = 71;
